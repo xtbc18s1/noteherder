3a63089d0471b74da38dc6128abbaa6589419fac
Dave Strus <dave@getfretless.com>
2018-05-22 15:09:48 -0400

    Add JSX to the Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
 const Sidebar = () => {
   return (
-    <div className="Sidebar">
-      Sidebar
-    </div>
+    <nav className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <a className="new-note" href="/notes">
+        <img src={newHover} alt="New note" />
+        <img className="outline" src={newIcon} alt="New note" />
+      </a>
+      <div className="SignOut">
+        <button>
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
+    </nav>
   )
 }
 
