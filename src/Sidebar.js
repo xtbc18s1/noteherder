3974b6f5db548e451c5fe995a353a84caf22791d
Dave Strus <dave@getfretless.com>
2018-05-22 15:29:13 -0400

    Begin styling the Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,9 +6,19 @@ import newHover from './new-hover.png'
 
 const Sidebar = () => {
   return (
-    <nav className="Sidebar">
-      <div className="logo">
-        <img src={quill} alt="Noteherder" />
+    <nav
+      className="Sidebar"
+      style={styles.sidebar}
+    >
+      <div
+        className="logo"
+        style={styles.logo}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          style={styles.logoImg}
+        />
       </div>
       <a className="new-note" href="/notes">
         <img src={newHover} alt="New note" />
@@ -21,6 +31,26 @@ const Sidebar = () => {
       </div>
     </nav>
   )
+}
+
+const styles = {
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
 }
 
 export default Sidebar
