6558af1b7fe2114d0875ec74b1fe07d4ffab989b
Dave Strus <dave@getfretless.com>
2018-05-22 14:44:59 -0400

    Style Main (using CSS in JS)

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,21 @@ import NoteForm from './NoteForm'
 
 const Main = () => {
   return (
-    <div className="Main">
+    <div
+      className="Main"
+      style={style}
+    >
       <Sidebar />
       <NoteList />
       <NoteForm />
     </div>
   )
+}
+
+const style = {
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'stretch',
 }
 
 export default Main
