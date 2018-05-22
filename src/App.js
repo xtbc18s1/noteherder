b6f48fcb55efd0f6d97a9f75c2e54149f6a8e850
Dave Strus <dave@getfretless.com>
2018-05-22 10:58:34 -0400

    Add Main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+
 import './App.css'
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Noteherder
+        <Main />
       </div>
     )
   }
