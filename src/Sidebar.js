c23d3606356340c9d987ab73122b8ef5f3b52737
Dave Strus <dave@getfretless.com>
2018-05-22 11:44:36 -0400

    Import images and add JSX for Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      Sidebar
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+
+      <a href="/notes">
+        <img
+          src={newHover}
+          alt="New note"
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+        />
+      </a>
+
+      <div className="SignOut">
+        <button>
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
     </div>
   )
 }
