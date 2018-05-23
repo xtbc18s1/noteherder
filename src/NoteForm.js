12e85f83c69ed35bb029e6934f2e584cd94dd54e
Dave Strus <dave@getfretless.com>
2018-05-23 14:20:52 -0400

    Style NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import './NoteForm.css'
+
 const NoteForm = () => {
   return (
     <div className="NoteForm">
-      NoteForm
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
+
+        <textarea name="body"></textarea>
+      </form>
     </div>
   )
 }
