aea4bcbe5296d8bbba7774d6a7be1f001c67b260
Dave Strus <dave@getfretless.com>
2018-05-23 14:37:34 -0400

    Add a key prop to each note

--- src/NoteList.js
+++ src/NoteList.js
@@ -26,7 +26,7 @@ const NoteList = () => {
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        { notes.map(note => <Note note={note} />) }
+        { notes.map(note => <Note key={note.id} note={note} />) }
       </ul>
     </div>
   )
