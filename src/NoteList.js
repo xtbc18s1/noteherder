d799230cbce8900ffc0ca67856e54e1fb19386cd
Dave Strus <dave@getfretless.com>
2018-05-23 09:50:50 -0400

    Style NoteList

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import './NoteList.css'
+
 const NoteList = () => {
   return (
     <div className="NoteList">
-      NoteList
-    </div>
+          <h3>Notes</h3>
+          <ul id="notes">
+            <a className="active">
+              <li>
+                <div className="note">
+                  <div className="note-title">
+                    Kohlrabi welsh
+                  </div>
+                  <div className="note-body">
+                    <p>
+                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </a>
+          </ul>
+        </div>
   )
 }
 
