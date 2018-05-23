7c556f0036a124575f6023c2f1145eba91be1616
Dave Strus <dave@getfretless.com>
2018-05-23 10:54:08 -0400

    Set the current note when clicking a note

--- src/Main.js
+++ src/Main.js
@@ -8,6 +8,7 @@ class Main extends React.Component {
   constructor() {
     super()
     this.state = {
+      currentNote: null,
       notes: [
         {
           id: 1,
@@ -28,11 +29,18 @@ class Main extends React.Component {
     }
   }
 
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
+
   render() {
     return (
       <div className="Main" style={style}>
         <Sidebar />
-        <NoteList notes={this.state.notes} />
+        <NoteList
+          notes={this.state.notes}
+          setCurrentNote={this.setCurrentNote}
+        />
         <NoteForm />
       </div>
     )
--- src/NoteList.js
+++ src/NoteList.js
@@ -9,7 +9,13 @@ const NoteList = (props) => {
       <h3>Notes</h3>
       <ul id="notes">
         <a className="active">
-          { props.notes.map(note => <Note key={note.id} note={note} />)}
+          { props.notes.map(note => (
+            <Note
+              key={note.id}
+              note={note}
+              setCurrentNote={props.setCurrentNote}
+            />
+          ))}
         </a>
       </ul>
     </div>
