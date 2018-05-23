fd6db1fac7e196be0366fab753ec327643922d51
Dave Strus <dave@getfretless.com>
2018-05-23 10:42:13 -0400

    Store notes in Main's state

--- src/Main.js
+++ src/Main.js
@@ -4,14 +4,39 @@ import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () => {
-  return (
-    <div className="Main" style={style}>
-      <Sidebar />
-      <NoteList />
-      <NoteForm />
-    </div>
-  )
+class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: [
+        {
+          id: 1,
+          title: 'Why I <3 JS',
+          body: 'Because I like code, and JS is code.',
+        },
+        {
+          id: 2,
+          title: 'Thoughts on breakfast',
+          body: 'I love it!',
+        },
+        {
+          id: 3,
+          title: 'Watching the first episode of Black Mirror with your parents',
+          body: 'Don\'t.',
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="Main" style={style}>
+        <Sidebar />
+        <NoteList notes={this.state.notes} />
+        <NoteForm />
+      </div>
+    )
+  }
 }
 
 const style = {
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,31 +3,13 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () => {
-  const notes = [
-    {
-      id: 1,
-      title: 'Why I <3 JS',
-      body: 'Because I like code, and JS is code.',
-    },
-    {
-      id: 2,
-      title: 'Thoughts on breakfast',
-      body: 'I love it!',
-    },
-    {
-      id: 3,
-      title: 'Watching the first episode of Black Mirror with your parents',
-      body: 'Don\'t.',
-    },
-  ]
-
+const NoteList = (props) => {
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
         <a className="active">
-          { notes.map(note => <Note key={note.id} note={note} />)}
+          { props.notes.map(note => <Note key={note.id} note={note} />)}
         </a>
       </ul>
     </div>
