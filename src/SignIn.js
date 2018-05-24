1c5d6717d1419c108b1fdca63ffac4f6c39c7bbd
Dave Strus <dave@getfretless.com>
2018-05-24 15:30:33 -0400

    Style SignIn

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import './SignIn.css'
+import quill from './quill.svg'
+import googleLogo from './google.svg'
+
 const SignIn = ({ handleAuth }) => {
   return (
     <div className="SignIn">
-      <button
-        onClick={handleAuth}
-      >Sign In</button>
+      <header className="Header">
+        <img src={quill} alt="" />
+        <span className="title">Noteherder</span>
+      </header>
+      <main>
+        <h3>Hey, Nerd! You Like Notes?</h3>
+        <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+        <button className="github" onClick={handleAuth}>
+          <i className="fab fa-github"></i>
+          Sign in with GitHub
+        </button>
+        <button className="google">
+          <img src={googleLogo} alt="" />
+          Sign in with Google
+        </button>
+      </main>
     </div>
   )
 }
