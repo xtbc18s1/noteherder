ec5ae0b524a2a81a273a030d373784abf670bd75
Dave Strus <dave@getfretless.com>
2018-05-24 14:34:18 -0400

    Add Firebase real-time database

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB9NOWvtiSapmeE6_FmYKnd8FmO9UoU_og",
+  authDomain: "notes18s1afternoon.firebaseapp.com",
+  databaseURL: "https://notes18s1afternoon.firebaseio.com",
+  projectId: "notes18s1afternoon",
+  storageBucket: "notes18s1afternoon.appspot.com",
+  messagingSenderId: "933182586296"
+}
+firebase.initializeApp(config)
