833433d36695bf8b156413d96c5fad751b013632
Dave Strus <dave@getfretless.com>
2018-05-24 12:13:55 -0400

    Update re-base config example

--- src/base.example.js
+++ src/base.example.js
@@ -1,7 +1,8 @@
 // Copy this file to `base.js` and add your app details
 
 import firebase from 'firebase/app'
-import database from 'firebase/database'
+import 'firebase/database'
+import 'firebase/auth'
 import Rebase from 're-base'
 
 // Initialize Firebase
@@ -13,7 +14,11 @@ const config = {
   storageBucket: "YOUR STORAGE BUCKET",
   messagingSenderId: "YOUR MESSAGING SENDER ID"
 }
+
 const app = firebase.initializeApp(config)
 const db = firebase.database(app)
+
+export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const auth = app.auth()
 
 export default Rebase.createClass(db)
