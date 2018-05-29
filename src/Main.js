25c85bdc3010cc81505eed6332939fb6e62aec51
Dave Strus <dave@getfretless.com>
2018-05-29 09:33:17 -0400

    Change endpoint where notes are synced

--- src/Main.js
+++ src/Main.js
@@ -15,7 +15,7 @@ class Main extends React.Component {
   }
 
   componentWillMount() {
-    base.syncState(`${this.props.uid}`, {
+    base.syncState(`notes/${this.props.uid}`, {
       context: this,
       state: 'notes',
       asArray: true,
